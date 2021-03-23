06ceab2d4a354ef4146aac34c6ce849a485c3594
TSA2COB <Atanu.Sarkar@in.bosch.com>
2021-03-24 01:23:34 +0530

    added JWT Token generator, Authentication Controller and JWT Util
    - AuthenticationRequest and AuthenticationResponse has been added under models
    - added /authenticate POST endpoint to generate JWT Tokens for valid users
    - PostMapping controller to generate JWT Tokens for valid users
    
    TODO:
    - still need to figure out how to integrate roles and principle
    - how to maintain session by storing the JWT in client side
    - how to pass JWT from client to the server on every http call inside request body to keep session alive and maintain principle

--- backend/src/main/java/com/bosch/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/com/bosch/backend/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ package com.bosch.backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,10 +38,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		// Authorization
-		http.authorizeRequests()
+		http.csrf().disable()
+			.authorizeRequests()
 			.antMatchers("/authors/**").hasRole("ADMIN")
 			.antMatchers("/books/**").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/authenticate").permitAll()
 			.antMatchers("/").permitAll()
 			.and().formLogin();
 	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
 }
