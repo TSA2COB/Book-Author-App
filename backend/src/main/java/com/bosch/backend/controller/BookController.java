ce616afe1fe03966cc8e7d5586e09ce944362b97
TSA2COB <Atanu.Sarkar@in.bosch.com>
2021-03-26 16:08:28 +0530

    added swagger ui
    - moved all REST endpoints from "root" to "/api"

--- backend/src/main/java/com/bosch/backend/controller/BookController.java
+++ backend/src/main/java/com/bosch/backend/controller/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bosch.backend.entities.Book;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api")
 public class BookController {
 	
 	@Autowired
